5b30147ad86f4dc62016fafe33f2b27d9b2a3fff
ArviinM <rhenmedina20@gmail.com>
2024-10-12 16:24:04 +0800

    chore: added to images constants the two pins

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,3 +1,5 @@
 export const IMAGES = {
   welcomeImage: require('../../assets/images/welcome_image.png'),
+  locationPin: require('../../assets/images/location_pin.png'),
+  locationPinRequest: require('../../assets/images/location_pin_request.png'),
 };
