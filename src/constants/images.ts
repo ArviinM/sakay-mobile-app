5e8bc9ef03ce4fc6d15d56940e0953dc9fcd57b4
ArviinM <rhenmedina20@gmail.com>
2024-10-14 11:50:18 +0800

    chore: added user location image

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -2,4 +2,5 @@ export const IMAGES = {
   welcomeImage: require('../../assets/images/welcome_image.png'),
   locationPin: require('../../assets/images/location_pin.png'),
   locationPinRequest: require('../../assets/images/location_pin_request.png'),
+  userLocationPin: require('../../assets/images/user_location_pin.png'),
 };
