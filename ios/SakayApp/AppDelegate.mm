ca79cbb5d11ffd41a1a510df2947275e4fc5a890
ArviinM <rhenmedina20@gmail.com>
2024-10-14 11:51:07 +0800

    chore: added google maps

--- ios/SakayApp/AppDelegate.mm
+++ ios/SakayApp/AppDelegate.mm
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 #import "RNBootSplash.h"
+#import <GoogleMaps/GoogleMaps.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"add_your_api_key"];
   self.moduleName = @"SakayApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
